00747791f735127ef2ddadb1363259b9151f3bbb
lithial <jameshessell@gmail.com>
2022-05-16 14:43:05 +1200

    updates to eslint and pretter and storybook configs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,11 @@
 import "!style-loader!css-loader!sass-loader!../src/styles/index.scss";
 
 export const parameters = {
-	actions: { argTypesRegex: "^on[A-Z].*" },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
-		},
-	},
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
 };
-
-export const decorators = [
-	(StoryFn, { globals: { height = 400 } }) => (
-		<>
-			<div style={{
-				height: `${height}px`
-			}}>
-				<StoryFn />
-			</div>
-		</>
-	),
-];
